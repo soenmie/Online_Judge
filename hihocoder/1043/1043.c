0573178bf3218a0bf6a8bdfae593b766be006bab
Sun Mian <msun@mobvoi.com>
2015-12-05 21:33:35 +0800

    hihocoder 1043

---
+++ hihocoder/1043/1043.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 100000
+
+#define max(a, b) ((a) > (b) ? (a) :(b))
+
+int dp[N + 1], n, m, i, j, c, v;
+
+int main() {
+    scanf("%d%d", &n ,&m);
+    for (i = 1; i <= n; ++i) {
+        scanf("%d%d", &c, &v);
+        for (j = c; j <= m; ++j) {
+            dp[j] = max(dp[j], dp[j - c] + v);
+        }
+    }
+    printf("%d\n", dp[m]);
+    return 0;
+}
