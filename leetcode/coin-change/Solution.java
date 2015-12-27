07821649e8b6ec12a392a4717fe6d067fdd913de
sunmian <sunmian@baidu.com>
2015-12-27 16:43:05 +0800

    leetcode coin-change

---
+++ leetcode/coin-change/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        int n = coins.length;
+        Arrays.fill(dp, -1);
+        Arrays.sort(coins);
+        dp[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = coins[i]; j <= amount; ++j) {
+                if (dp[j - coins[i]] != -1) {
+                    dp[j] = dp[j] == -1 ? dp[j - coins[i]] + 1 :
+                        Math.min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
